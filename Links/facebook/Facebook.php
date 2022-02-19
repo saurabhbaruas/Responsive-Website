14fe38d13d6ad2cdf430a1c71340a0261be8a851
saurabhbaruas <saurabhrwt27@gmail.com>
2022-02-19 18:49:16 +0530

    First commit

---
+++ Links/facebook/Facebook.php
@@ -0,0 +1,287 @@
+<html>
+    <head>
+  <title>Facebook</title>
+        <link href="home.css" rel="stylesheet" type="text/css" />
+        <script>
+            function L()
+            {
+                alert("LOL!!!(;");
+            }
+            function F()
+            {
+                alert("Yor're My best friend!!!");
+            }
+            function M()
+            {
+                alert("I wanna say that I adore you. I don't know why but I can not Say!!");
+            }
+            function N()
+            {
+                document.write("Do You Know Mr. Saurabh Singh Rawat??");
+                alert("Welcome!!@");
+            }
+            function S()
+            {
+                alert("I'm Not a Scrodriver who will change your Setting!!!");
+            }
+            function E()
+            {
+                alert("o,oo!!You can't Edit the profile,Due to some security.(:");
+            }
+            function P()
+            {
+                alert("Your Post is in processig.Thanks!!");
+            }
+        </script>
+    </head>
+    <body>
+        <div class="header1">
+        </div>
+          <div id="img3" class="header1">
+            <img src="img/fb.jpg" height="50px"/>
+        </div>
+        <div id="searcharea" class="header1">
+            <input placeholder="Saurabh Singh Rawat...." type="text"   id="searchbox" /></div>
+        
+
+                          <div id="searching" class="header1"><a href="#" onclick="L()">Search</a></div>
+        
+        
+        
+                   <div id="profilearea" class="header1">
+                     <a href="Biography.html"><img src="img/aadi.gif" height="54px"</a></div>
+                          <div id="profilearea1" class="header1"><a href="index.html">Profile</a></div>
+                           <div id="profilearea3" class="header1">|</div>
+                            <div id="profilearea4" class="header1"><a href="Facebook.php">Home</a></div>
+                              <div id="findf" class="header1"><a href="#" onclick="F()">Friends</a></div>
+                               <div id="message" class="header1"><a href="#" onclick="M()">Message</a></div>
+                              <div id="notification" class="header1"><a href="#" onclick="N()">Notifications</a></div>
+                          <div id="profilearea2" class="header1">|</div>
+                      <div id="setting" class="header1"><a href="#" onclick="S()">Setting</a></div>
+                <div id="logout" class="header1"><a href="#" onclick="L()">LogOut</a></div>
+          
+            <div class="bodyn">
+               <div id="side1" class="bodyn"><img src="img/s.jpg" height="30px" />Profile</div>
+                <div id="side2" class="bodyn" <a href="#" onclick="E()"> Edit profile</a></div>
+                      <div id="side3" class="bodyn"> News feed </div>
+                          <div id="side4" class="bodyn"> Messages </div>
+                             <div id="side5" class="bodyn"> Events </div>
+                               <div id="side6" class="bodyn"> Pages </div>
+                                   <div id="side7" class="bodyn"> Pages feed </div>
+                                       <div id="side8" class="bodyn"> Like Pages </div>
+                                          <div id="side9" class="bodyn"> Creat pages </div>
+                                              <div id="side10" class="bodyn"> Creat ad </div>
+                                                  <div id="side11" class="bodyn"> GROUPS </div>
+                                                     <div id="side12" class="bodyn"> New groups </div>
+                                                        <div id="side13" class="bodyn"> Creat group </div>
+                                                     <div id="side14" class="bodyn"> APP </div>
+                                                  <div id="side15" class="bodyn"> Games </div>
+                                                <div id="side16" class="bodyn"> On this day </div>
+                                             <div id="side17" class="bodyn"> Ganes feed </div>
+                                           <div id="side18" class="bodyn"> Friends </div>
+                                         <div id="side19" class="bodyn"> Close Friends </div>
+                                      <div id="side20" class="bodyn"> Family </div>
+                                    <div id="side21" class="bodyn"> INTERESTS </div>
+                                 <div id="side22" class="bodyn"> Pages and public </div>
+                               <div id="side23" class="bodyn"> EVENTS </div>
+                            <div id="side24" class="bodyn"> Creat events </div>
+                         <div id="side25" class="bodyn"> Devloped By: </div>
+                      <div id="side26" class="bodyn"> Mr. Saurabh Singh Rawat</divid>
+            </div>
+              <div class="post00">
+            </div>
+            <div class="post10">
+            </div><div class="post20">
+            </div><div class="post30">  
+            </div><div class="post40">  
+            </div> <div class="post50">   
+            </div><div class="post60">  
+            </div><div class="post70">  
+            </div><div class="post80">  
+
+            </div>
+            <div class="header0001"></div>
+              <div class="sideboxxx"> </div>
+            <div class="sideboxxxx2"></div>
+            
+            <div class="post">
+                <div id="column-1" class="post">update status | add photos/videos | create photo album<hr><br><br><br><br><br><br><br><br><hr></div>
+                <div id="column-1" class="post">update status | add photos/videos | create photo album<hr><br><br><br><br><br><br><br><br><hr></div>
+            
+            <div id="postpos" class="post"><input type="submit" id="buttonpost" onclick="P()" value="post" /></div>
+                
+            <div id="postboxpos" class="post"><textarea placeholder="What's in your mind?" id="postbox" type="text"></textarea></div>
+            </div>
+            
+            <div class="post1"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><img src="img/Mere%20Papa.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Kuldeep Bartwal,Jeenuu</p1><p2> and</p2><p1> 2,869 others</p1><p2> like this page</p2>
+            
+            <div id="post2text" class="post1"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Dharam Singh Rawat</p1><p2> and</p2><p1> 478 others.</p1><br><p4>4 hrs.</p4><br><p2>At the office of his father in New Delhi,India.</p2><br><p>i love you Dad....</p>
+            </div>
+            
+            <div id="commentpro62" class="post1"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos2" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>
+            
+          <div class="post2"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/meri%20maa.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Sumitra Rawat,Arti Bhandari</p1><p2> and</p2><p1> 1,269 also</p1><p2> like this page</p2>
+            
+            <div id="post3text" class="post2"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Sumitra Rawat</p1><p2> and</p2><p1> 478 others.</p1><br><p4>9 hrs.</p4><br><p2>At the tample of Lord Shiva in Uttrakhand,India.</p2><br><p> love you Mom...(:</p>
+            </div>
+            
+            <div id="commentprof3" class="post2"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos3" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>
+                       
+                       
+            <div class="post3"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/a%20d.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Dharam Singh Rawat,Mehandra singh Rawat</p1><p2> and</p2><p1> 1,869 others</p1><p2> like this page</p2>
+            
+            <div id="post33text" class="post3"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Arti Bhandari</p1><p2> and</p2><p1> 767 others.</p1><br><p4>Yesterday.</p4><br><p2>At the tample of his village in Uttrakhand,India.</p2><br><p>Missiiingg uuhhh deeeeee!!!!!!!.</p>
+            </div>
+            
+            <div id="commentprof33" class="post3"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos33" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>
+                       
+                       
+<div class="post4"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/p%20d.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Arti Bhandari,Kamal Singh Bhandari</p1><p2> and</p2><p1> 1,469 others</p1><p2> like this page</p2>
+            
+            <div id="post4text" class="post4"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Pooja Chauhan</p1><p2> and</p2><p1> 857 others.</p1><br><p4>The day before Yesterday.</p4><br><p2>At Mayur Bihar,Phase 3 in New Delhi ,India.</p2><br><p>Missiiingg uuhhh deeeeee!!!!!!!.</p>
+            </div>
+            
+            <div id="commentprof4" class="post4"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos4" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>                       
+
+                       
+<div class="post5"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/adi2.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Arti Bhandari,Kamal Singh Bhandari,Sumitra Rawat</p1><p2> and</p2><p1> 2,769 others</p1><p2> like this page</p2>
+            
+            <div id="post5text" class="post5"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Aditi Bhandari</p1><p2> and</p2><p1> 557 others.</p1><br><p4>December 10,2016</p4><br><p2>At Ridraprayag in Uttrakhand,India.</p2><br><p>Mising you my Adorable Sweet NIECE...(;!!!!!!!.</p>
+            </div>
+            
+            <div id="commentprof5" class="post5"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos5" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>
+                       
+                       
+<div class="post6"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/kuldeep.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Manish Teneza,Kailash Singh Bisht,Deepak Negi</p1><p2> and</p2><p1> 2,769 others</p1><p2> also like this page</p2>
+            
+            <div id="post6text" class="post6"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Kuldeep Bartwal</p1><p2> and</p2><p1> 3587 others.</p1><br><p4>March 19,2016</p4><br><p2>At Ridraprayag in Uttrakhand,India.</p2><br><p>Enjoying Collage LIFE...</p>
+            </div>
+            
+            <div id="commentprof6" class="post6"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos6" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>                       
+
+                       
+                       
+<div class="post7"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/kailash.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Manish Teneza,Kailash Singh Bisht,Kuldeep Bartwal,Deepak Negi</p1><p2> and</p2><p1> 1,969 others</p1><p2> also like this page</p2>
+            
+            <div id="post7text" class="post7"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Kailash Singh Bisht</p1><p2> and</p2><p1> 4597 others.</p1><br><p4>April  9,2017</p4><br><p2>At Ridraprayag in Uttrakhand,India.</p2><br><p>Thank you soo muchh God to give a Very prosperous life!!!</p>
+            </div>
+            
+            <div id="commentprof7" class="post7"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos7" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>                       
+
+                       
+                       
+                       
+<div class="post8"><img src="img/s.jpg" height="40px"/><br><br><br><br><br><br><img src="img/depu.jpg" width="580px"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Manish Teneza,Kailash Singh Bisht,Kuldeep Bartwal,Deepak Negi</p1><p2> and</p2><p1> 1,969 others</p1><p2> also like this page</p2>
+            
+            <div id="post8text" class="post8"><p3>Saurabh Singh Rawat</p3><p2> with</p2><p1> Deepak Negi,Himanshu soyal</p1><p2> and</p2><p1> 9597 others.</p1><br><p4>May  3,2017</p4><br><p2>Dubai,India.</p2><br><p>Do wants to Become The Software Engineer??</p>
+            </div>
+            
+            <div id="commentprof8" class="post8"><img src="img/s.jpg" height="30px" /></div>
+            
+            <div id="commentboxpos8" class="post1"><input type="textarea" placeholder="Write comment" id="commentbox"/></div>
+            </div>         
+                       
+                       
+                       
+                       
+                       
+                       
+        <div class="sidebox">
+        <div id="sidebox1" class="sidebox">
+             <div id="sideboxx1">YOUR PAGES</div><hr><br><br><br>See all<hr>
+            <div id="sideboxx2">This Week</div><br><br><br>See more<hr>
+            <div id="sideboxx3">Recent Posts</div><br><br><br>See more<hr>
+            <div id="sideboxx4">You haven't posted in these days</div><br><br><br><br><br><br>See all
+            </div>  
+    
+            
+        <div id="post1pos" class="sidebox"><input type="submit" id="buttonpost1" value="Write a post" /></div> 
+            </div>
+        <div class="sideboxxx2">
+            <div id="sidebox2" class="sideboxxx2">
+            <br>
+                <hr>
+                <div id="sideboxx21">Trending</div>
+                <br><br><br>See more<hr>
+                <div id="sideboxx22">Suggested Page</div><br><br><br>See all<hr>
+              <div id="sideboxx23">People you may know</div><br><br><br><br><br><br>See all
+            </div>
+            
+            <div class="chat-sidebarx"></div>
+            <div class="chat-sidebarx"><div id="chatnamebox" class="chat-sidebar">Dharam Singh Rawat</div><br>
+            <div id="chatnamebox1" class="chat-sidebar">Sumitra Rawat</div>
+            <div id="chatnamebox2" class="chat-sidebar">Rishi Bhushan</div>
+            <div id="chatnamebox3" class="chat-sidebar">Manoj Jain</div>
+            <div id="chatnamebox4" class="chat-sidebar">Arti Bhandari </div>
+            <div id="chatnamebox5" class="chat-sidebar">Kuldeep Chauhan</div>
+            <div id="chatnamebox6" class="chat-sidebar">Pooja chauhan</div>
+             <div id="chatnamebox7" class="chat-sidebar">Kuldeep Bartwal</div>
+            <div id="chatnamebox8" class="chat-sidebar">Kailash singh bisht</div>
+            <div id="chatnamebox9" class="chat-sidebar">Deepak Negi</div>
+            <div id="chatnamebox10" class="chat-sidebar">Tarun Bisht</div>
+            <div id="chatnamebox11" class="chat-sidebar">Sumit Negi</div>
+            <div id="chatnamebox12" class="chat-sidebar">Kamal Bhandari</div>
+            <div id="chatnamebox13" class="chat-sidebar">Sandeep chauhan</div>
+            <div id="chatnamebox14" class="chat-sidebar">Pawan Rawat</div>
+             
+
+            <div id="chatnameboxp1" class="chat-sidebar"><img src="img/Papa.jpg" alt="profile" width="35px" /></div>
+            <div id="chatnameboxp2" class="chat-sidebar"><img src="img/mom.jpg" alt="profile" width="25px" /></div>
+            <div id="chatnameboxp3" class="chat-sidebar"><img src="img/bhushan.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp4" class="chat-sidebar"><img src="img/jain's.jpg" width="35px" alt="profile" /></div>
+            <div id="chatnameboxp5" class="chat-sidebar"><img src="img/a%20d.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp6" class="chat-sidebar"><img src="img/saurabh1.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp7" class="chat-sidebar"><img src="img/p%20d.jpg" width="25px" alt="profile" /></div>
+           <div id="chatnameboxp8" class="chat-sidebar"><img src="img/kuldeep.jpg" width="20px" alt="profile" /></div>
+            <div id="chatnameboxp9" class="chat-sidebar"><img src="img/JEENU.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp10" class="chat-sidebar"><img src="img/deepak.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp11" class="chat-sidebar"><img src="img/tarun.jpg" width="25" alt="profile" /></div>
+            <div id="chatnameboxp12" class="chat-sidebar"><img src="img/sumit.jpg" width="30px" alt="profile" /></div>
+            <div id="chatnameboxp13" class="chat-sidebar"><img src="img/saurabh.jpg" width="25px" alt="profile" /></div>
+            <div id="chatnameboxp14" class="chat-sidebar"><img src="img/jigs.jpg" width="40px" alt="profile" /></div>
+            <div id="chatnameboxp15" class="chat-sidebar"><img src="img/pawan.jpg" width="25px" alt="profile" /></div>
+
+
+            </div>
+            </div>    
+    </body>
+</html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
